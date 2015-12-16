7e0a2ff06f8a764a3b68a8cae9a66e9a734d88c7
tyty5949 <tyty5949@gmail.com>
2015-12-15 18:13:42 -0600

    Javadoc'ed the GLFrame

--- src/com/swinggl/elements/GLFrame.java
+++ src/com/swinggl/elements/GLFrame.java
@@ -41,8 +41,12 @@ import static org.lwjgl.system.MemoryUtil.NULL;
 /**
  * Created on 12/13/2015.
  * http://www.glfw.org/docs/latest/index.html
+ * <p/>
+ * This elements houses everything necessary to make a complete window in LWJGL using GLFW. It doesn't simplify much and knowledge of LWJGL and GLFW are still
+ * required. This can be thought of as a wrapper of LWJGL which wraps OpenGL and GLFW.
+ * <p/>
  * ------------------------------------------
- * GLFW
+ * GLFW TODO
  * [X] Callbacks
  * [X] Cursors
  * [ ] Cursor Object (Cursor look)
@@ -103,18 +107,37 @@ public class GLFrame {
 
     private GLPanel currentGameState;
 
+    /**
+     * Constructor that builds a default GLFrame
+     */
     public GLFrame() {
         this(false, NULL);
     }
 
+    /**
+     * Constructor that builds a default GLFrame which links graphics data with another GLFrame
+     *
+     * @param secondWindowHandle - The other window handle that this GLFrame will be linked to.
+     */
     public GLFrame(long secondWindowHandle) {
         this(false, secondWindowHandle);
     }
 
+    /**
+     * Constructor that builds a default fullscreen GLFrame
+     *
+     * @param fullscreen - Sets whether or not the frame is fullscreen
+     */
     public GLFrame(boolean fullscreen) {
         this(fullscreen, NULL);
     }
 
+    /**
+     * Constructor that builds a default fullscrene GLFrame that links graphics data with another GLFrame
+     *
+     * @param fullscreen         - Sets whether or not the frame is fullscreen
+     * @param secondWindowHandle - The other window handle that this GLFrame will be linked to.
+     */
     public GLFrame(boolean fullscreen, long secondWindowHandle) {
         System.setProperty("java.awt.headless", "true");
         Thread.currentThread().setName("SwingGL | render");
@@ -138,6 +161,9 @@ public class GLFrame {
         scrollCallback = new Mouse.Scroll();
     }
 
+    /**
+     *
+     */
     public void run() {
         if (fullscreen) {
             window = glfwCreateWindow(windowWidth, windowHeight, title, glfwGetPrimaryMonitor(), secondWindowHandle);
@@ -267,75 +293,140 @@ public class GLFrame {
         return this;
     }
 
+    /**
+     * Locks the mouse to the center of the screen and make the cursor invisible
+     */
     public void disableMouse() {
         mouseDisabled = true;
         if (window != 0L)
             glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
     }
 
+    /**
+     * Makes the cursor invisible
+     */
     public void hideMouse() {
         mouseHidden = true;
         if (window != 0L)
             glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
     }
 
+    /**
+     * Makes the mouse visible again (sets the mouse to its default state)
+     */
     public void showMouse() {
         mouseHidden = false;
         if (window != 0L)
             glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
     }
 
+    /**
+     * Makes the mouse visible again (sets the mouse to its default state)
+     */
     public void enableMouse() {
         mouseDisabled = false;
         if (window != 0L)
             glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
     }
 
+    /**
+     * Closes the GLFrame and cleans up resources
+     */
     public void exit() {
         glfwSetWindowShouldClose(window, GL_TRUE);
     }
 
+    /**
+     * Minimizes the window
+     */
     public void iconify() {
         if (window != 0L)
             glfwIconifyWindow(window);
     }
 
+    /**
+     * Tells whether or not the window is fullscreen
+     *
+     * @return - Whether or not the window is fullscreen
+     */
     public boolean isFullscreen() {
         return fullscreen;
     }
 
+    /**
+     * Tells whether or not the window is minimized
+     *
+     * @return - Whether or not the window is minimized
+     */
     public int isIconified() {
         return glfwGetWindowAttrib(window, GLFW_ICONIFIED);
     }
 
+    /**
+     * Tells whether or not the window is in focus. A window is in focus if it was the last window that recieved input.
+     *
+     * @return - GLFW_TRUE if the window is in focus and GLFW_FALSE otherwise
+     */
     public int isInFocus() {
         return glfwGetWindowAttrib(window, GLFW_FOCUSED);
     }
 
+
+    /**
+     * Checks if a joystick button or axis is present
+     *
+     * @param joystick - The joystick to check
+     * @return - GLFW_TRUE if the the joystick exists and GLFW_FALSE otherwise
+     */
     public int isJoystickPresent(int joystick) {
         if (window != 0L)
             return glfwJoystickPresent(joystick);
         else
             Debug.println("GLFW Window must be initialized first!", Debug.ANSI_YELLOW);
-        return 0;
+        return GLFW_FALSE;
     }
 
+    /**
+     * Tells whether or not the mouse is disabled
+     *
+     * @return - true if mouse is disabled and false otherwise
+     */
     public boolean isMouseDisabled() {
         return mouseDisabled;
     }
 
+    /**
+     * Tells whether or not the mouse is hidden
+     *
+     * @return - true if mouse is hidden and false otherwise
+     */
     public boolean isMouseHidden() {
         return mouseHidden;
     }
 
+    /**
+     * Tells whether or not the window is visible
+     *
+     * @return - true if window is visible and false otherwise
+     */
     public boolean isVisible() {
         return visible;
     }
 
+    /**
+     * Gives the background color as a java.awt.Color
+     *
+     * @return - Gives the background color
+     */
     public Color getBackgroundColor() {
         return backgroundColor;
     }
 
+    /**
+     * Gets the text that is currently stored in the clipboard
+     *
+     * @return - The text in the clipboard
+     */
     public String getClipboardString() {
         if (window != 0L)
             return glfwGetClipboardString(window);
@@ -344,6 +435,12 @@ public class GLFrame {
         return "";
     }
 
+    /**
+     * Gets the data about the position of the axis of the selected joystick
+     *
+     * @param joystick - The joystick
+     * @return - The data the axis gives about it's position
+     */
     public FloatBuffer getJoystickAxes(int joystick) {
         if (window != 0L)
             glfwGetJoystickAxes(joystick);
@@ -352,6 +449,12 @@ public class GLFrame {
         return null;
     }
 
+    /**
+     * Gets the data about the selected joystick button
+     *
+     * @param joystick - The joystick
+     * @return - The data the button gives about it's position
+     */
     public ByteBuffer getJoystickButtons(int joystick) {
         if (window != 0L)
             return glfwGetJoystickButtons(joystick);
@@ -360,6 +463,12 @@ public class GLFrame {
         return null;
     }
 
+    /**
+     * Gets the physical name of the joystick axis or button so that it can be identified on the device
+     *
+     * @param joystick - The joystick
+     * @return - The name of the selected joystick
+     */
     public String getJoystickName(int joystick) {
         if (window != 0L)
             return glfwGetJoystickName(joystick);
@@ -368,24 +477,49 @@ public class GLFrame {
         return "";
     }
 
+    /**
+     * Gives the position of the top-left corner of the window as a java.awt.Point
+     *
+     * @return - The window position
+     */
     public Point getPosition() {
         return new Point(windowX, windowY);
     }
 
+    /**
+     * Gives the size fo the window from frame edge to edge as a java.awt.Dimension
+     *
+     * @return - The size of the window
+     */
     public Dimension getSize() {
         return new Dimension(windowWidth, windowHeight);
     }
 
+    /**
+     * Gives the title of the window as it appears to the OS
+     *
+     * @return - The title of the window
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Sets the background color of the window
+     *
+     * @param color - The background color
+     */
     public void setBackgroundColor(Color color) {
         backgroundColor = color;
         if (window != 0L)
             GL11.glClearColor(color.getRed() / 255f, color.getGreen() / 255f, color.getBlue() / 255f, color.getAlpha() / 255f);
     }
 
+    /**
+     * Sets the text data that is saved in the clipboard
+     *
+     * @param clipboardString - The text to be saved
+     */
     public void setClipboardString(String clipboardString) {
         if (window != 0L)
             glfwSetClipboardString(window, clipboardString);
@@ -393,30 +527,55 @@ public class GLFrame {
             Debug.println("GLFW Window must be initialized first!", Debug.ANSI_YELLOW);
     }
 
+    /**
+     * Sets the major version of OpenGL that is to be used by the window
+     *
+     * @param majorVersion - The major version
+     */
     public void setContextVersionMajor(int majorVersion) {
         if (window != 0L)
             throw new RuntimeException("GLFW window already initialized, cannot set majorVersion");
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, majorVersion);
     }
 
+    /**
+     * Sets the minor version of OpenGL that is to be used by the window
+     *
+     * @param minorVersion - The minor version
+     */
     public void setContextVersionMinor(int minorVersion) {
         if (window != 0L)
             throw new RuntimeException("GLFW window already initialized, cannot set minorVersion");
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, minorVersion);
     }
 
+    /**
+     * Sets the GLFWCursorEnterCallback for the window
+     *
+     * @param cursorEnterCallback - The callback
+     */
     public void setCursorEnterCallback(GLFWCursorEnterCallback cursorEnterCallback) {
         this.cursorEnterCallback = cursorEnterCallback;
         if (window != 0)
             glfwSetCursorEnterCallback(window, cursorEnterCallback);
     }
 
+    /**
+     * Sets the GLFWCursorPosCallback for the window (default com.swinggl.backend.Mouse)
+     *
+     * @param cursorPosCallback - The callback
+     */
     public void setCursorPosCallback(GLFWCursorPosCallback cursorPosCallback) {
         this.cursorPosCallback = cursorPosCallback;
         if (window != 0L)
             glfwSetCursorPosCallback(window, cursorPosCallback);
     }
 
+    /**
+     * Whether or not the window is to be decorated. A decorated window has a frame, icon, title, and close buttons on it, a undecorated frame does not.
+     *
+     * @param decorated - Whether or not the window will be decorated
+     */
     public void setDecorated(boolean decorated) {
         if (window != 0L)
             throw new RuntimeException("GLFW window already initialized, cannot set decorated");
@@ -426,45 +585,86 @@ public class GLFrame {
             glfwWindowHint(GLFW_DECORATED, GLFW_FALSE);
     }
 
+    /**
+     * Sets the GLFWDropCallback for the window. The dropcallback is invoked when a file is dropped onto the window
+     *
+     * @param dropCallback - The callback
+     */
     public void setDropCallback(GLFWDropCallback dropCallback) {
         if (window != 0)
             glfwSetDropCallback(window, dropCallback);
     }
 
+    /**
+     * Sets the GLFWKeyCallback for the window
+     *
+     * @param keyCallback - The callback
+     */
     public void setKeyCallback(GLFWKeyCallback keyCallback) {
         this.keyCallback = keyCallback;
         if (window != 0L)
             glfwSetKeyCallback(window, keyCallback);
     }
 
+    /**
+     * Sets the GLFWMonitorCallback for the window
+     *
+     * @param monitorCallback - The callback
+     */
     public void setMonitorCallback(GLFWMonitorCallback monitorCallback) {
         glfwSetMonitorCallback(monitorCallback);
     }
 
+    /**
+     * Sets the GLFWMouseButtonCallback for the window
+     *
+     * @param mouseButtonCallback - The callback
+     */
     public void setMouseButtonCallback(GLFWMouseButtonCallback mouseButtonCallback) {
         this.mouseButtonCallback = mouseButtonCallback;
         if (window != 0L)
             glfwSetMouseButtonCallback(window, mouseButtonCallback);
     }
 
+    /**
+     * Sets the level of multisampling the window uses for rendering
+     *
+     * @param samples - The number of samples
+     */
     public void setMultisampling(int samples) {
         if (window != 0L)
             throw new RuntimeException("GLFW window already initialized, cannot send new samples");
         glfwWindowHint(GLFW_SAMPLES, samples);
     }
 
+    /**
+     * Sets the GLPanel that the window is currently rendering and updating. The render thread will call initialize() the frame after it is added.
+     *
+     * @param panel - The new GLPanel
+     */
     public void setPanel(GLPanel panel) {
         if (currentGameState != null)
             currentGameState.dispose();
         currentGameState = panel;
     }
 
+    /**
+     * Sets the window to a position on the screen. Use the GLFrame constants for this method (WINDOW_CENTERED, WINDOW_BOTTOM_LEFT, etc)
+     *
+     * @param position - The GLFrame constant for the position
+     */
     public void setPosition(int position) {
         this.windowPosition = position;
         if (window != 0L && !fullscreen)
             updateWindowPosition();
     }
 
+    /**
+     * Sets a manual position of the top-left corner of the window.
+     *
+     * @param windowX - The x position of the window
+     * @param windowY - The y position of the window
+     */
     public void setPosition(int windowX, int windowY) {
         this.windowX = windowX;
         this.windowY = windowY;
@@ -473,12 +673,22 @@ public class GLFrame {
             glfwSetWindowPos(window, windowX, windowY);
     }
 
+    /**
+     * Sets the refresh rate of the window. Refresh rates will be limited to the physical capabilities of the monitor.
+     *
+     * @param refreshRate - The refresh rate
+     */
     public void setRefreshRate(int refreshRate) {
         if (window != 0L)
             throw new RuntimeException("GLFW window already initialized, cannot send new refreshRate");
         glfwWindowHint(GLFW_REFRESH_RATE, refreshRate);
     }
 
+    /**
+     * Sets whether or not the window will be resizable
+     *
+     * @param resizable - Whether or not the window is resizable
+     */
     public void setResizable(boolean resizable) {
         if (window != 0L)
             throw new RuntimeException("GLFW window already initialized, cannot send new resizable");
@@ -488,12 +698,23 @@ public class GLFrame {
             glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
     }
 
+    /**
+     * Sets the GLFWScrollCallback for the window. (default com.swinggl.backend.Mouse)
+     *
+     * @param scrollCallback - The callback
+     */
     public void setScrollCallback(GLFWScrollCallback scrollCallback) {
         this.scrollCallback = scrollCallback;
         if (window != 0L)
             glfwSetScrollCallback(window, scrollCallback);
     }
 
+    /**
+     * Sets the size of the window from frame edge to edge. If the window is fullscreen some sizes are limited by graphical capabilities.
+     *
+     * @param width  - The width of the window
+     * @param height - The height of the window
+     */
     public void setSize(int width, int height) {
         windowWidth = width;
         windowHeight = height;
@@ -516,22 +737,42 @@ public class GLFrame {
         }
     }
 
+    /**
+     * Sets the FPS target for the render loop
+     *
+     * @param targetFPS - The target FPS
+     */
     public void setTargetFPS(int targetFPS) {
         this.targetFPS = targetFPS;
         renderNS = 1000000000.0 / targetFPS;
     }
 
+    /**
+     * Sets the UPS target for the update loop
+     *
+     * @param targetUPS - The target UPS
+     */
     public void setTargetUPS(int targetUPS) {
         this.targetUPS = targetUPS;
         updateNS = 1000000000.0 / targetUPS;
     }
 
+    /**
+     * Sets the title shown by the window.
+     *
+     * @param title - The window title
+     */
     public void setTitle(String title) {
         this.title = title;
         if (window != 0L)
             glfwSetWindowTitle(window, title);
     }
 
+    /**
+     * Sets whether or notthe window to be visible on the screen (the icon will still be visible on task bars when not visible)
+     *
+     * @param visible - Whether or not the window is to be visible
+     */
     public void setVisible(boolean visible) {
         this.visible = visible;
         if (window != 0L) {
@@ -547,36 +788,66 @@ public class GLFrame {
         }
     }
 
+    /**
+     * Sets the GLFWWindowCloseCallback for the window
+     *
+     * @param windowCloseCallback - The callback
+     */
     public void setWindowCloseCallback(GLFWWindowCloseCallback windowCloseCallback) {
         this.windowCloseCallback = windowCloseCallback;
         if (window != 0L)
             glfwSetWindowCloseCallback(window, windowCloseCallback);
     }
 
+    /**
+     * Sets the GLFWWindowFocusCallback for the window
+     *
+     * @param windowFocusCallback - The callback
+     */
     public void setWindowFocusCallback(GLFWWindowFocusCallback windowFocusCallback) {
         this.windowFocusCallback = windowFocusCallback;
         if (window != 0L)
             glfwSetWindowFocusCallback(window, windowFocusCallback);
     }
 
+    /**
+     * Sets the GLFWWindowIconifyCallback for the window
+     *
+     * @param windowIconifyCallback - The callback
+     */
     public void setWindowIconifyCallback(GLFWWindowIconifyCallback windowIconifyCallback) {
         this.windowIconifyCallback = windowIconifyCallback;
         if (window != 0L)
             glfwSetWindowIconifyCallback(window, windowIconifyCallback);
     }
 
+    /**
+     * Sets the GLFWWindowPosCallback for the window
+     *
+     * @param windowPosCallback - The callback
+     */
     public void setWindowPosCallback(GLFWWindowPosCallback windowPosCallback) {
         this.windowPosCallback = windowPosCallback;
         if (window != 0L)
             glfwSetWindowPosCallback(window, windowPosCallback);
     }
 
+    /**
+     * Sets the GLFWWindowRefreshCallback for the window
+     *
+     * @param windowRefreshCallback - The callback
+     */
     public void setWindowRefreshCallback(GLFWWindowRefreshCallback windowRefreshCallback) {
         this.windowRefreshCallback = windowRefreshCallback;
         if (window != 0L)
             glfwSetWindowRefreshCallback(window, windowRefreshCallback);
     }
 
+    /**
+     * Sets the GLFWWindowSizeCallback for the window
+     *
+     * @param windowSizeCallback - The callback
+     */
     public void setWindowSizeCallback(GLFWWindowSizeCallback windowSizeCallback) {
         this.windowSizeCallback = windowSizeCallback;
         if (window != 0L)
